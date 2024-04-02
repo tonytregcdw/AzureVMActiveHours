b9d7cab3dac9de8ec446a09d1c10f0e69d681727
tonytregcdw <54070632+tonytregcdw@users.noreply.github.com>
2024-04-02 17:37:48 +0100

    tenant

--- functionTimerRestartVM/run.ps1
+++ functionTimerRestartVM/run.ps1
@@ -1,12 +1,17 @@
 # Input bindings are passed in via param block.
 param($Timer)
 
-$Subscriptions = Get-AzSubscription -TenantId $tenantId | Where-Object { $_.Name -like "hov-sub*" } | select Id, Name
+$tenant = get-aztenant
+$tenantID = $tenant.Id
+
+$Subscriptions = Get-AzSubscription -TenantId $tenantId | select Id, Name
 $date = [System.TimeZoneInfo]::ConvertTimeBySystemTimeZoneId([DateTime]::Now,"GMT Standard Time")
 
 foreach ($Subscription in $Subscriptions) {
 	Set-AzContext -Subscription $Subscription.Id
+    write-output "Looking up VMs in sub $($Subscription.Name)"
 	foreach ($vm in get-azvm -status | Where-Object {($_.Tags.RestartTime -ne $null)}) {
+        write-output "Checking restart tags for VM $($vm.Name)"
         $now = $date
         if (($vm.PowerState -eq 'VM running') -and ($now -gt $(get-date $($vm.tags.RestartTime))) -and ($now -lt $(get-date $($vm.tags.RestartTime)).Addminutes(10))) {
             #Restart-AzVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -NoWait
